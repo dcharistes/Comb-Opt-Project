5d17b8ee57c23e5ccc3680e871bc598573c836d6
ChrisPara21 <paraschakischristos@gmail.com>
2025-11-16 14:36:20 +0200

    Add files via upload

--- gvrppgenerator.py
+++ gvrppgenerator.py
@@ -3,8 +3,9 @@ import numpy
 import argparse
 import os
 
+
+
 V=0
-CU=0
 D=0
 K=0 #num of vehicles
 num_clusters=0
@@ -13,6 +14,7 @@ grid_size=0
 CUSTOMER_POINT=1
 DEPOT_POINT=2
 num_clusters=0
+PR=0
 
 class Point:
     def __init__(self, x=0, y=0):
@@ -27,6 +29,10 @@ def add_depot(grid, points):
     points[0].y = random.randint(0, grid_size - 1)
 
     grid[points[0].x][points[0].y] = DEPOT_POINT
+
+    depot_index = points[0].x * grid_size + points[0].y
+
+    return depot_index
 
 
 # Function to add Customers to grid 
@@ -114,7 +120,7 @@ def add_Demand(clusters):
 
 def save_on_disk(grid, clusters, A, D, points, filename="0"):
 
-    folder_name = str(V) + "_" + str(CU) + "_" + str(num_clusters)
+    folder_name = str(grid_size) + "_" + str(V) + "_" + str(num_clusters)
     file_extension = ".txt"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
@@ -125,7 +131,7 @@ def save_on_disk(grid, clusters, A, D, points, filename="0"):
         print("--Writing problems on disk... (step 6)")
 
         text = list()
-        text.append(f"{grid_size*grid_size} {CU} {num_clusters} {K} {Q}\n")
+        text.append(f"{grid_size*grid_size} {V} {num_clusters} {K} {Q}\n")
 
         cluster_ids = [cid for cid in clusters.keys()]
         demand_dict = {cid: d for cid, d in D}
@@ -160,5 +166,70 @@ def save_on_disk(grid, clusters, A, D, points, filename="0"):
         print(f"\nError while creating file {filename+file_extension}!\n\n")
         
 
+# Function that handles the input arguments from the user
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Generalized Vehicle Routing Problem Generator\nUsage: <grid size> <total Vertices (V)> <clusters (num_clusters))> <number of Vehicles (K)> <Capacity of each vehicle (Q)> <# problems>"
+    )
+
+    # Define each positional argument
+    parser.add_argument("grid_size", type=int, help="Size of the grid (e.g., 10 for a 10x10 grid)")
+    parser.add_argument("V", type=int, help="Total number of Vertices (V), basically the total number of customers - 1")
+    parser.add_argument("num_clusters", type=int, help="Number of clusters (num_clusters)")
+    parser.add_argument("K", type=int, help="Total number of available identical vehicles (K)")
+    parser.add_argument("Q", type=int, help="Max capacity of each vehicle (Q)")
+    parser.add_argument("num_problems", type=int, help="Number of problems")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    global grid_size, V, num_clusters, K, Q, PR
+    grid_size = args.grid_size
+    V = args.V
+    num_clusters = args.num_clusters
+    K = args.K
+    Q = args.Q
+    PR = args.num_problems
+
+# To use the parser
+if __name__ == "__main__":
+
+    # Handle input arguments
+    args = parse_arguments()
+    
+    for i in range(PR):
+        # Define grid
+        grid = numpy.zeros(shape=(grid_size,grid_size), dtype=int)
+        
+        # Define points list
+        points = [Point() for i in range(V)]
+
+        
+        add_customers(grid, points)
+        depot_index = add_depot(grid, points)
+
+        clusters = create_clusters(points, depot_index, num_clusters)
+
+        A = conectivity(points, clusters)
+
+        D = add_Demand(clusters)
+
+
+        # Generate (x, y) coordinates for all points in the grid
+        #point_coords = numpy.array([(i // grid_size, i % grid_size) for i in range(grid_size * grid_size)])
+        
+
+
+        
+        # Save problem on disk
+        save_on_disk(grid, clusters, A, D, points, str(i))
+
+        
+        # Delete data structures
+        del grid
+        del clusters
+        del A
+        del D
+        del points
 
         
